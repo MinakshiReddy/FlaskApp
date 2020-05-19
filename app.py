8eba264fded9e89e0665d5b536471d888cd09865
Tej <tejasmane1710@gmail.com>
2020-05-19 15:33:23 +0530

    Sign Up Commit

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ def main():
 def extras():
     return render_template('extras.html')
 
+@app.route("/signup")
+def signup():
+    return render_template('signup.html')
+
 if __name__ == "__main__":
     app.run()
 
